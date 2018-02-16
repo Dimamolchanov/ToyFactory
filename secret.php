d06e8faca7bc45634490e404b406ea9765ce5b9a
Egor Balanovich <Egor_Balanovich@sephora.com>
2018-02-16 19:07:23 +0300

    add php file

---
+++ secret.php
@@ -0,0 +1,17 @@
+<?php
+
+if ($_GET['horse']==10){
+    echo 'L';
+}
+if ($_GET[look]==5&&$_GET[meadow]==23){
+    echo 'O';}
+if ($_GET[dew]==6&&$_GET[mount]==64){
+    echo 'N';}
+if ($_GET[bond]==24&&$_GET[march]==40){
+    echo 'D';}
+if ($_GET[wise]==43&&$_GET[strong]==4&&$_GET[curve]==29){
+    echo 'O';}
+if ($_GET[fast]==31&&$_GET[clever]==76&&$_GET[moon]==20){
+    echo 'N';}
+
+?>
